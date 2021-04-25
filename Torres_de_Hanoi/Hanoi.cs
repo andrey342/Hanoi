99c1f0ac2a1cdaac1f530ad66e2f4d583ccae1ea
andrey342 <61473165+andrey342@users.noreply.github.com>
2021-04-26 00:19:14 +0200

    Implementado algoritmo iterativo.

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -61,8 +61,67 @@ namespace Torres_de_Hanoi
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
-        }
+            int movimientos = 0; //variable donde almaceno los movimientos hechos
+            bool solucion = false; //bool para salir del while o no
+            if (n % 2 == 0)
+            { //si es par
+                while (solucion == false)
+                { //mientras que no hay solucion
+
+                    /*------- Movimientos + aumento contador  ---------*/
+                    movimientos++;
+                    mover_disco(ini, aux);
+                    Console.WriteLine("Mover de ini a aux");
+
+                    movimientos++;
+                    mover_disco(ini, fin);
+                    Console.WriteLine("Mover de ini a fin");
+
+                    movimientos++;
+                    mover_disco(aux, fin);
+                    Console.WriteLine("Mover de aux a fin");
+
+                    /*------- FIN -> Movimientos + aumento contador ---------*/
+                    if (fin.Size.Equals(n))
+                    {
+                        solucion = true;
+                    }
+                }
+            }
+            else
+            { //si es impar
+                while (solucion == false)
+                {
+
+                    /*------- Movimientos + aumento contador  ---------*/
+
+
+                    mover_disco(ini, fin); //como es impar hacemos que el primer movimiento sea ini -> fin
+                    Console.WriteLine("Mover de ini a fin");
+                    movimientos++;
+                    if (fin.Size.Equals(n)) //si coincide
+                    {
+                        solucion = true;
+                        break; //salir bucle
+                    }
+
+                    mover_disco(ini, aux);
+                    Console.WriteLine("Mover de ini a aux");
+                    movimientos++;
+
+                    mover_disco(aux, fin);
+                    Console.WriteLine("Mover de aux a fin");
+                    movimientos++;
+
+                    /*------- FIN -> Movimientos + aumento contador ---------*/
+                    if (fin.Size.Equals(n))
+                    {
+                        solucion = true;
+                    }
+                }
+            }
+            return movimientos;
+        }s
 
     }
 }
